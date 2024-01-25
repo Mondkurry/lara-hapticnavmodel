1c39e4065070c7ed468f537a3aa9d1f49fe6ff79
Aryan Mondkar <aryan.mondkar@gmail.com>
2024-01-24 18:21:14 -0800

    initial prototyping everything is unfinished

---
+++ includes/visualization.h
@@ -0,0 +1,12 @@
+#ifndef VISUALIZATION_H 
+#define VISUALIZATION_H 
+
+class visualization{
+  public: 
+    int num_rows;
+    int num_cols;
+
+    visualization(int num_rows, int num_cols);
+}
+
+#endif
